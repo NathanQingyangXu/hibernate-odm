93b90f82f10d62d461f79c7c43dbb508f822ce3e
nathan xu <nathan.qingyang.xu@gmail.com>
2025-08-22 15:59:30 -0400

    add logging to AbstractCloseable.kt

--- odm-core/src/main/kotlin/org/hibernate/odm/jdbc/AbstractCloseable.kt
+++ odm-core/src/main/kotlin/org/hibernate/odm/jdbc/AbstractCloseable.kt
@@ -1,18 +1,26 @@
 package org.hibernate.odm.jdbc
 
+import io.github.oshai.kotlinlogging.KotlinLogging
+
 internal abstract class AbstractCloseable {
+    companion object {
+        @JvmStatic
+        private val logger = KotlinLogging.logger {}
+    }
+
     private var closed: Boolean = false
 
     fun isClosed() = closed
 
-    @Suppress("all")
+    @Suppress("TooGenericExceptionCaught")
     fun close() {
         if (!closed) {
             try {
                 closeActually()
                 closed = true
             } catch (e: Exception) {
-                //todo logging in error level
+                logger.error { "failed to close $this: ${e.message}" }
+                throw e
             }
         }
     }
