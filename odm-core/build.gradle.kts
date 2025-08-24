1914ef74119c7bac244ead3add2371dd60cdf1b1
Nathan Xu <nathan.qingyang.xu@gmail.com>
2025-08-24 09:37:22 -0400

    use ktfmt

--- odm-core/build.gradle.kts
+++ odm-core/build.gradle.kts
@@ -3,90 +3,79 @@ import org.apache.tools.ant.filters.ReplaceTokens
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    `java-library`
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.detekt)
+  `java-library`
+  alias(libs.plugins.kotlin.jvm)
+  alias(libs.plugins.detekt)
+  alias(libs.plugins.ktfmt)
 }
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
+java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Integration Test
 
 sourceSets {
-    create("integrationTest") {
-        compileClasspath += sourceSets.main.get().output
-        runtimeClasspath += sourceSets.main.get().output
-    }
+  create("integrationTest") {
+    compileClasspath += sourceSets.main.get().output
+    runtimeClasspath += sourceSets.main.get().output
+  }
 }
 
 val integrationTestSourceSet: SourceSet = sourceSets["integrationTest"]
 
 val integrationTestImplementation: Configuration by
-configurations.getting { extendsFrom(configurations.implementation.get()) }
+    configurations.getting { extendsFrom(configurations.implementation.get()) }
 val integrationTestRuntimeOnly: Configuration by
-configurations.getting { extendsFrom(configurations.runtimeOnly.get()) }
+    configurations.getting { extendsFrom(configurations.runtimeOnly.get()) }
 
 val integrationTestTask =
     tasks.register<Test>("integrationTest") {
-        group = LifecycleBasePlugin.VERIFICATION_GROUP
-        testClassesDirs = integrationTestSourceSet.output.classesDirs
-        classpath = integrationTestSourceSet.runtimeClasspath
+      group = LifecycleBasePlugin.VERIFICATION_GROUP
+      testClassesDirs = integrationTestSourceSet.output.classesDirs
+      classpath = integrationTestSourceSet.runtimeClasspath
     }
 
 tasks.check { dependsOn(integrationTestTask) }
 
 tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
-    testLogging { events(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED) }
+  useJUnitPlatform()
+  testLogging { events(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED) }
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Dependencies
 
 dependencies {
+  testImplementation(libs.bundles.test.common)
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    testImplementation(libs.bundles.test.common)
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  integrationTestImplementation(libs.bundles.test.common)
+  integrationTestRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    integrationTestImplementation(libs.bundles.test.common)
-    integrationTestRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  integrationTestImplementation(libs.hibernate.testing) {
+    exclude(group = "org.apache.logging.log4j", module = "log4j-core")
+  }
 
-    integrationTestImplementation(libs.hibernate.testing) {
-        exclude(group = "org.apache.logging.log4j", module = "log4j-core")
-    }
+  api(libs.hibernate.orm.core)
+  api(libs.mongodb.driver.sync)
 
-    api(libs.hibernate.orm.core)
-    api(libs.mongodb.driver.sync)
-
-    implementation(libs.sl4j.api)
-    implementation(libs.logback.classic)
-    implementation(libs.kotlin.logging)
+  implementation(libs.sl4j.api)
+  implementation(libs.logback.classic)
+  implementation(libs.kotlin.logging)
 }
-
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Static Analysis
 
 detekt {
-    buildUponDefaultConfig = true
-    allRules = false
-    config.setFrom("$rootDir/config/detekt.yml")
+  buildUponDefaultConfig = true
+  allRules = false
+  config.setFrom("$rootDir/config/detekt.yml")
 }
 
-tasks.withType<Detekt>().configureEach {
-    reports {
-        html.required = true
-    }
-}
+tasks.withType<Detekt>().configureEach { reports { html.required = true } }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // processResources token replacement
@@ -95,7 +84,8 @@ val mongoDriverName = libs.mongodb.driver.sync.get().name
 val mongoDriverVersion = libs.versions.mongodb.driver.sync.get()
 
 tasks.processResources {
-    filesMatching("**/driver.properties") {
-        filter<ReplaceTokens>("tokens" to mapOf("driver.name" to mongoDriverName, "driver.version" to mongoDriverVersion))
-    }
+  filesMatching("**/driver.properties") {
+    filter<ReplaceTokens>(
+        "tokens" to mapOf("driver.name" to mongoDriverName, "driver.version" to mongoDriverVersion))
+  }
 }
--- odm-core/src/integrationTest/kotlin/org/hibernate/odm/SmokeTest.kt
+++ odm-core/src/integrationTest/kotlin/org/hibernate/odm/SmokeTest.kt
@@ -1,14 +1,13 @@
 package org.hibernate.odm
 
+import kotlin.test.Test
 import org.hibernate.testing.orm.domain.helpdesk.Ticket
 import org.hibernate.testing.orm.junit.DomainModel
 import org.hibernate.testing.orm.junit.SessionFactory
-import kotlin.test.Test
 
 @DomainModel(annotatedClasses = [Ticket::class])
 @SessionFactory
 class SmokeTest {
 
-    @Test
-    fun `test Hibernate odm could be launched successfully`() {}
+  @Test fun `test Hibernate odm could be launched successfully`() {}
 }
